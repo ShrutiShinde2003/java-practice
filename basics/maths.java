fd32aff28a41d9d687e84e1cb56793d9dd55470b
ShrutiShinde2003 <shrutivshinde2140@gmail.com>
2026-06-06 11:45:13 +0530

    Added math class

---
+++ basics/maths.java
@@ -0,0 +1,13 @@
+public class maths {
+    public static void main (String[]args){
+        System.out.println(Math.max(10,5));
+        System.out.println(Math.min(7,8));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-8));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.round(3.14));
+        System.out.println(Math.ceil(6.76));
+        System.out.println(Math.floor(5.78));
+        System.out.println(Math.random());
+    }
+}
